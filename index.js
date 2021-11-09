103051969cd174e2211aa1fe1ee0415d5e4c79d5
Gokce Gokmen <51439620+A01258386@users.noreply.github.com>
2021-11-08 21:45:42 -0800

    some errors fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const expressLayouts = require("express-ejs-layouts");
 const session = require("express-session");
 const path = require("path");
 const port = process.env.port || 8000;
-
+const fs = require("fs");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -25,6 +25,7 @@ const passport = require("./middleware/passport");
 const { authRouter, reminderRouter } = require("./routes")
 
 const upload = require("./middleware/upload");
+const checkAuth = require("./middleware/checkAuth");
 
 // Middleware for express
 app.use(express.json());
@@ -52,12 +53,11 @@ app.use((req, res, next) => {
   next()
 });
 
-app.post("/upload", upload.single("file"), async (req, res) => {
+app.post("/upload",  upload.single("file"), async (req, res) => {
   try {
-    console.log(req.file);
     const url = await require("imgur").uploadFile(`./uploads/${req.file.filename}`)
-    res.json({ url: url });
-    fs.unlinkSync(`./uploads/${file.filename}`);
+    res.json({ url: url.link });
+    fs.unlinkSync(`./uploads/${req.file.filename}`);
   } catch (err) {
     console.log(err);
   }
